517b9e6cfc4294f1f87a7d6dea1ebaf300411f51
Carlosorre02 <carlo.sorrentino@studenti.unitn.it>
2024-05-23 10:45:29 +0200

    solve some problem

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const core = require("@actions/core");
+const axios = require("axios");
 
 const reportPath = core.getInput("trivy-report");
 
@@ -8,7 +9,7 @@ if (!reportPath) {
     process.exit(1);
 }
 
-fs.readFile(reportPath, "utf8", (err, data) => {
+fs.readFile(reportPath, "utf8", async (err, data) => {
     if (err) {
         core.setFailed(`Error reading the report: ${err.message}`);
         process.exit(1);
@@ -37,6 +38,29 @@ fs.readFile(reportPath, "utf8", (err, data) => {
                 });
             }
         });
+
+        // Estrai namespace e repository dal campo ArtifactName
+        const [namespace, repoTag] = report.ArtifactName.split("/");
+        const [repository] = repoTag.split(":");
+        const digest = report.Metadata.ImageID;
+
+        // Call Docker Hub API to get tags
+        const url = `https://hub.docker.com/v2/namespaces/${namespace}/repositories/${repository}/images/${digest}/tags?page=1&page_size=20`;
+        core.info(`Fetching tags from: ${url}`);
+
+        try {
+            const response = await axios.get(url);
+            const tags = response.data.results;
+
+            core.info("Tags:");
+            tags.forEach(tag => {
+                core.info(`  Tag: ${tag.tag}, Is Current: ${tag.is_current}`);
+            });
+        } catch (apiErr) {
+            core.setFailed(`Error fetching tags from Docker Hub: ${apiErr.message}`);
+            process.exit(1);
+        }
+
     } catch (parseErr) {
         core.setFailed(`Error parsing the report: ${parseErr.message}`);
         process.exit(1);
