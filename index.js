d329cebf5620cee1b2e2cf322d2438f243869295
Carlosorre02 <113434882+Carlosorre02@users.noreply.github.com>
2024-10-12 10:29:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if (!reportPath) {
 
 fs.readFile(reportPath, "utf8", async (err, data) => {
     if (err) {
-        core.setFailed(`Error reading the report: ${err.message}`);
+        core.setFailed(Error reading the report: ${err.message});
         process.exit(1);
     }
 
@@ -30,7 +30,7 @@ fs.readFile(reportPath, "utf8", async (err, data) => {
         }
 
         // Log dell'immagine base
-        core.info(`Base Image: ${artifactName}`);
+        core.info(Base Image: ${artifactName});
 
         // Funzione per estrarre informazioni rilevanti da ogni vulnerabilità
         const extractVulnInfo = (vulnerabilities) => {
@@ -48,22 +48,17 @@ fs.readFile(reportPath, "utf8", async (err, data) => {
 
         // Iterare attraverso i risultati del report
         report.Results.forEach(result => {
-            core.info(`Target: ${result.Target}`);
+            core.info(Target: ${result.Target});
             const relevantVulns = extractVulnInfo(result.Vulnerabilities || []);
 
-            if (relevantVulns.length > 0) {
-                relevantVulns.forEach(vulnInfo => {
-                    core.info(`Package: ${vulnInfo.PkgName}`);
-                    core.info(`Vulnerability ID: ${vulnInfo.VulnerabilityID}`);
-                    core.info(`Severity: ${vulnInfo.Severity}`);
-                    core.info(`Installed Version: ${vulnInfo.InstalledVersion}`);
-                    core.info(`Fixed Version: ${vulnInfo.FixedVersion}`);
-                    core.info("---");
-                });
-            } else {
-                // Se non ci sono vulnerabilità, visualizza il messaggio
-                core.info(`Nessuna vulnerabilità trovata per ${result.Target}`);
-            }
+            relevantVulns.forEach(vulnInfo => {
+                core.info(Package: ${vulnInfo.PkgName});
+                core.info(Vulnerability ID: ${vulnInfo.VulnerabilityID});
+                core.info(Severity: ${vulnInfo.Severity});
+                core.info(Installed Version: ${vulnInfo.InstalledVersion});
+                core.info(Fixed Version: ${vulnInfo.FixedVersion});
+                core.info("---");
+            });
         });
 
         // Usare ArtifactName per determinare il namespace e il repository
@@ -77,12 +72,12 @@ fs.readFile(reportPath, "utf8", async (err, data) => {
             repository = parts[1];
         }
 
-        core.info(`Namespace: ${namespace}`);
-        core.info(`Repository: ${repository}`);
+        core.info(Namespace: ${namespace});
+        core.info(Repository: ${repository});
 
         // Funzione per ottenere tutti i tag che contengono "alpine" e attraversare le pagine
         const getAlpineTags = async (namespace, repository, currentTag) => {
-            let url = `https://hub.docker.com/v2/repositories/${namespace}/${repository}/tags/?name=alpine&page_size=100`;
+            let url = https://hub.docker.com/v2/repositories/${namespace}/${repository}/tags/?name=alpine&page_size=100;
             let tags = [];
 
             while (url) {
@@ -111,7 +106,7 @@ fs.readFile(reportPath, "utf8", async (err, data) => {
                     // Se c'è una pagina successiva, aggiornare l'URL per ottenere la pagina successiva
                     url = response.data.next;
                 } catch (apiErr) {
-                    core.setFailed(`Error fetching tags from Docker Hub: ${apiErr.message}`);
+                    core.setFailed(Error fetching tags from Docker Hub: ${apiErr.message});
                     process.exit(1);
                 }
             }
@@ -143,54 +138,50 @@ fs.readFile(reportPath, "utf8", async (err, data) => {
         if (alpineTags.length > 0) {
             const sortedTags = sortTags(alpineTags);
             core.info("Alpine Tags ordinati:");
-            sortedTags.forEach(tag => core.info(`  Tag: ${tag}`));
+            sortedTags.forEach(tag => core.info(  Tag: ${tag}));
 
             // Scansione delle prime 5 immagini
             const top5Images = sortedTags.slice(0, 5);
 
             const trivyScan = async (image) => {
-                const fullImageName = `library/node:${image}`; // Aggiungi il prefisso corretto
+                const fullImageName = library/node:${image}; // Aggiungi il prefisso corretto
                 return new Promise((resolve, reject) => {
-                    exec(`trivy image --format json --output trivy-report-${image}.json --severity MEDIUM,HIGH,CRITICAL ${fullImageName}`, (error, stdout, stderr) => {
+                    exec(trivy image --format json --output trivy-report-${image}.json --severity MEDIUM,HIGH,CRITICAL ${fullImageName}, (error, stdout, stderr) => {
                         if (error) {
-                            reject(`Errore durante la scansione di Trivy per l'immagine ${fullImageName}: ${stderr}`);
+                            reject(Errore durante la scansione di Trivy per l'immagine ${fullImageName}: ${stderr});
                         } else {
-                            if (fs.existsSync(`trivy-report-${image}.json`)) {
-                                resolve(`Trivy report per ${fullImageName} salvato.`);
-                            } else {
-                                reject(`Il report per ${fullImageName} non esiste.`);
-                            }
+                            resolve(Trivy report per ${fullImageName} salvato.);
                         }
                     });
                 });
             };
 
             const parseTrivyReport = (image) => {
-                const reportPath = `trivy-report-${image}.json`;
+                const reportPath = trivy-report-${image}.json;
                 const reportData = fs.readFileSync(reportPath, "utf8");
                 const report = JSON.parse(reportData);
 
                 report.Results.forEach(result => {
-                    core.info(`Target: ${result.Target}`);
+                    core.info(Target: ${result.Target});
                     const vulnerabilities = result.Vulnerabilities || [];
 
                     if (vulnerabilities.length > 0) {
                         vulnerabilities.forEach(vuln => {
-                            core.info(`Package: ${vuln.PkgName}`);
-                            core.info(`Vulnerability ID: ${vuln.VulnerabilityID}`);
-                            core.info(`Severity: ${vuln.Severity}`);
-                            core.info(`Installed Version: ${vuln.InstalledVersion}`);
-                            core.info(`Fixed Version: ${vuln.FixedVersion || "No fix available"}`);
+                            core.info(Package: ${vuln.PkgName});
+                            core.info(Vulnerability ID: ${vuln.VulnerabilityID});
+                            core.info(Severity: ${vuln.Severity});
+                            core.info(Installed Version: ${vuln.InstalledVersion});
+                            core.info(Fixed Version: ${vuln.FixedVersion || "No fix available"});
                             core.info("---");
                         });
                     } else {
-                        core.info(`Nessuna vulnerabilità trovata per ${result.Target}`);
+                        core.info(Nessuna vulnerabilità trovata per ${result.Target});
                     }
                 });
             };
 
             for (const image of top5Images) {
-                core.info(`Inizio scansione per immagine: ${image}`); // Log inizio scansione
+                core.info(Inizio scansione per immagine: ${image});
                 try {
                     await trivyScan(image);
 
@@ -198,18 +189,17 @@ fs.readFile(reportPath, "utf8", async (err, data) => {
                     parseTrivyReport(image);
 
                     // Aggiungi un ritardo di 10 secondi tra le scansioni
-                    await sleep(2000); // 10000 millisecondi = 10 secondi
+                    await sleep(10000); // 10000 millisecondi = 10 secondi
                 } catch (err) {
-                    core.setFailed(`Errore nella scansione di ${image}: ${err}`);
+                    core.setFailed(Errore nella scansione di ${image}: ${err});
                 }
-                core.info(`Fine scansione per immagine: ${image}`); // Log fine scansione
             }
         } else {
             core.info("Non sono stati trovati tag Alpine più recenti.");
         }
 
     } catch (parseErr) {
-        core.setFailed(`Error parsing the report: ${parseErr.message}`);
+        core.setFailed(Error parsing the report: ${parseErr.message});
         process.exit(1);
     }
 });
